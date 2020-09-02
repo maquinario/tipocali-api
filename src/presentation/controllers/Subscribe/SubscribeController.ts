58531b526565fcff21280381f6179327e1627a09
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-01 22:48:25 -0300

    feat: ensure SubscribeController returns 400 if an invalid email is provided

--- src/presentation/controllers/Subscribe/SubscribeController.ts
+++ src/presentation/controllers/Subscribe/SubscribeController.ts
@@ -2,14 +2,22 @@ import { HttpRequest, HttpResponse } from '../../protocols/Http'
 import { MissingParamError } from '../../errors/MissingParamError'
 import { badRequest } from '../../helpers/HttpHelper'
 import Controller from '../../protocols/Controller'
+import { EmailValidator } from '../../protocols/EmailValidator'
+import { InvalidParamError } from '../../errors/InvalidParamError'
 
 export class SubscribeController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {}
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
