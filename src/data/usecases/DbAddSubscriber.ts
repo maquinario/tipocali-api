3d3f3298ba467b2e53d09900f29551567a33003d
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-02 11:09:26 -0300

    feat: ensure DbAddSubscriber returns a subscriber on success

--- src/data/usecases/DbAddSubscriber.ts
+++ src/data/usecases/DbAddSubscriber.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/return-await */
 import AddSubscriber, { AddSubscriberModel } from '../../domain/usecases/AddSubscriber'
 import SubscriberModel from '../../domain/models/Subscriber'
 import { AddSubscriberRepository } from '../protocols/AddSubscriberRepository'
@@ -6,7 +7,7 @@ export class DbAddSubscriber implements AddSubscriber {
   constructor (private readonly addSubscriberRepository: AddSubscriberRepository) {}
 
   async add (subscriberData: AddSubscriberModel): Promise<SubscriberModel> {
-    await this.addSubscriberRepository.add(subscriberData)
-    return await new Promise(resolve => resolve(null))
+    const subscriber = await this.addSubscriberRepository.add(subscriberData)
+    return subscriber
   }
 }
