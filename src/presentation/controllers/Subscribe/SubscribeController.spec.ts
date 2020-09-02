69082341d36e9dd24daba86eaa9a54418846568a
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-01 22:52:14 -0300

    test: ensure EmailValidator is called with correct email

--- src/presentation/controllers/Subscribe/SubscribeController.spec.ts
+++ src/presentation/controllers/Subscribe/SubscribeController.spec.ts
@@ -3,6 +3,7 @@ import { SubscribeController } from './SubscribeController'
 import { MissingParamError } from '../../errors/MissingParamError'
 import { InvalidParamError } from '../../errors/InvalidParamError'
 import { EmailValidator } from '../../protocols/EmailValidator'
+import { HttpRequest } from '../../protocols/Http'
 
 interface SutTypes {
   sut: SubscribeController
@@ -21,6 +22,13 @@ const makeSut = (): SutTypes => {
     sut, emailValidatorStub
   }
 }
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+    email: faker.internet.email()
+  }
+})
 
 describe('Subscribe Controller', () => {
   test('Should return 400 if no name is provided', () => {
@@ -59,5 +67,13 @@ describe('Subscribe Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = makeFakeRequest()
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
   })
 })
