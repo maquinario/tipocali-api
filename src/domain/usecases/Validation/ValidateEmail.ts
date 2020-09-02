73767a4c955508962829623422366a2896838b52
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-02 10:14:06 -0300

    feat: ensure ValidateEmail returns true if isValid returns true

--- src/domain/usecases/Validation/ValidateEmail.ts
+++ src/domain/usecases/Validation/ValidateEmail.ts
@@ -2,6 +2,8 @@ import { EmailValidator } from '../../../presentation/protocols'
 
 export default class ValidateEmail implements EmailValidator {
   isValid (email: string): boolean {
-    return false
+    // eslint-disable-next-line no-useless-escape
+    const exp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return exp.test(String(email).toLowerCase())
   }
 }
