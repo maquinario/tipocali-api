4bebabe65f57ec79462eaa9154bf378574136e58
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-01 22:48:37 -0300

    test: ensure SubscribeController returns 400 if an invalid email is provided

--- src/presentation/controllers/Subscribe/SubscribeController.spec.ts
+++ src/presentation/controllers/Subscribe/SubscribeController.spec.ts
@@ -1,14 +1,30 @@
 import faker from 'faker'
 import { SubscribeController } from './SubscribeController'
 import { MissingParamError } from '../../errors/MissingParamError'
+import { InvalidParamError } from '../../errors/InvalidParamError'
+import { EmailValidator } from '../../protocols/EmailValidator'
 
-const makeSut = (): SubscribeController => {
-  return new SubscribeController()
+interface SutTypes {
+  sut: SubscribeController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SubscribeController(emailValidatorStub)
+  return {
+    sut, emailValidatorStub
+  }
 }
 
 describe('Subscribe Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: faker.internet.email()
@@ -20,7 +36,7 @@ describe('Subscribe Controller', () => {
   })
 
   test('Should return 400 if no email is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: faker.name.firstName()
@@ -29,5 +45,19 @@ describe('Subscribe Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if an invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        email: faker.internet.email()
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })
