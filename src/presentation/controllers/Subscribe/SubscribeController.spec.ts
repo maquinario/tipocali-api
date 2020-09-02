299265225df9752017c95e1779306d9cd9411055
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-01 23:07:16 -0300

    refactor: mock implementation change

--- src/presentation/controllers/Subscribe/SubscribeController.spec.ts
+++ src/presentation/controllers/Subscribe/SubscribeController.spec.ts
@@ -17,15 +17,6 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      throw new Error()
-    }
-  }
-  return new EmailValidatorStub()
-}
-
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new SubscribeController(emailValidatorStub)
@@ -84,8 +75,10 @@ describe('Subscribe Controller', () => {
   })
 
   test('Should return 500 if EmailValidator throws', () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SubscribeController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
     const httpRequest = makeFakeRequest()
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
