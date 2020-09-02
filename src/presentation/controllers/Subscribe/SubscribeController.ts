b71a9095137d0f2154bdb0b02272eea24f0469e1
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-01 22:57:20 -0300

    feat: ensure SubscribeController returns 500 if EmailValidator throws

--- src/presentation/controllers/Subscribe/SubscribeController.ts
+++ src/presentation/controllers/Subscribe/SubscribeController.ts
@@ -4,20 +4,28 @@ import { badRequest } from '../../helpers/HttpHelper'
 import Controller from '../../protocols/Controller'
 import { EmailValidator } from '../../protocols/EmailValidator'
 import { InvalidParamError } from '../../errors/InvalidParamError'
+import { ServerError } from '../../errors/ServerError'
 
 export class SubscribeController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
