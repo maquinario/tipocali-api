21d0a2518644b5ce762cb5d2519a4a8d87ac39ae
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-02 12:15:15 -0300

    feat: ensure SubscriberMongoRepository returns a subscriber on success

---
+++ src/infra/db/mongodb/Subscriber.ts
@@ -0,0 +1,14 @@
+import { AddSubscriberRepository } from '../../../data/protocols/AddSubscriberRepository'
+import { AddSubscriberModel } from '../../../domain/usecases/AddSubscriber'
+import SubscriberModel from '../../../domain/models/Subscriber'
+import { MongoHelper } from './helpers/MongoHelper'
+
+export class SubscriberMongoRepository implements AddSubscriberRepository {
+  async add (subscribeData: AddSubscriberModel): Promise<SubscriberModel> {
+    const subscriberCollection = MongoHelper.getCollection('subscribers')
+    const result = await subscriberCollection.insertOne(subscribeData)
+    const subscriber = result.ops[0]
+    const { _id, ...subsciberWithoutId } = subscriber
+    return Object.assign({}, subsciberWithoutId, { id: _id })
+  }
+}
