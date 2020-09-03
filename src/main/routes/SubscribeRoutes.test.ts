850c5a192456f3158cc8db0839131caf1611f307
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-09-03 11:53:29 -0300

    refactor: connect to MongoDB before integration tests

--- src/main/routes/SubscribeRoutes.test.ts
+++ src/main/routes/SubscribeRoutes.test.ts
@@ -1,8 +1,21 @@
 import request from 'supertest'
 import app from '../config/app'
 import faker from 'faker'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/MongoHelper'
 
 describe('Subscribe Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const subscriberCollection = MongoHelper.getCollection('subscribers')
+    await subscriberCollection.deleteMany({})
+  })
   test('Should return a subscriber on success', async () => {
     await request(app)
       .post('/api/subscribe')
